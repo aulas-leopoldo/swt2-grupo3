ae3b3384f98b9a1e4692e8746d8f7e55b06c0a04
Rafael Bernardo <rafaelbnneto@gmail.com>
2025-02-19 15:05:10 -0300

    adicionar funcao de subracao

--- scm.c
+++ scm.c
@@ -4,6 +4,11 @@ void soma(int a, int b) {
     printf("%d + %d = %d\n", a, b, a + b);
 }
 
+void subtrai(int a, int b) {
+    if(a - b < 0) printf("A subtracao nao pode resultar num numero menor que zero!\n");
+    else printf("%d - %d = %d\n", a, b, a - b);
+}
+
 void multiplica(int a, int b) {
     printf("%d x %d = %d\n", a, b, a * b);
 }
