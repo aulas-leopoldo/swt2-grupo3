3b93bfa57a8f66e2676e8b1d69181b64a767c112
p-r-m-n <paulo_rmn2010@hotmail.com>
2025-02-19 15:08:02 -0300

    Adicionada a modificação de nome de usuário

--- scm.c
+++ scm.c
@@ -9,7 +9,10 @@ void multiplica(int a, int b) {
 }
 
 int main() {
-    printf("Olá, Estudante!\n");
+    char user_name[30];
+    printf("Digite o nome do usuário: ");
+    scanf("%s", user_name);
+    printf("Olá, %s!\n", user_name);
     soma(5, 3);
     multiplica(5, 3);
     return 0;
